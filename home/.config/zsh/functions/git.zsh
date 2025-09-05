6589b408f7dd473f37b6c6c40af465d236de190a
K2nate <k2nate.dev@gmail.com>
2025-09-05 23:20:05 +0900

    chore: add ghq config and alias

--- home/.config/zsh/functions/git.zsh
+++ home/.config/zsh/functions/git.zsh
@@ -95,3 +95,15 @@ gpr() {
         gh pr create --fill
     fi
 }
+
+# ghq fuzzy finder
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^g' ghq-fzf
